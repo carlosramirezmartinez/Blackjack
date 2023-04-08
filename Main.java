1d3f53f9c5c22f20b10ee7220f425f40a01a2565
Carlos Ramirez <92943401+carlosramirezmartinez@users.noreply.github.com>
2023-04-08 18:30:08 +0200

    Blackjack
    Queremos crear el juego de blackjack contra la cpu

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+Para coger una carta se debera pulsar Si o Yes(S/s o Y/y) o No(n/n)
+
+*/
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    
+    private static int PartidasJugadas = 0;
+    private static int Victorias = 0;
+
+ 
+    }
+    
